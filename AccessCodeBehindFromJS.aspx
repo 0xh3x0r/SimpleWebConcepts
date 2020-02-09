c6377ce70730b94ea2d846a0ede45ba4d1d91cd2
Junior Yoda <0xh3x0r@gmail.com>
2020-02-10 00:00:54 +0300

    Calling a code behind function from JS
    Accessing a static function with javascript

---
+++ AccessCodeBehindFromJS.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+
+
+    <form id="form1" runat="server">
+            <asp:ScriptManager ID="ScriptManager1" runat="server" EnablePageMethods="true">
+</asp:ScriptManager>
+
+    <div>
+    <script>
+        function cside(x)
+        {
+            PageMethods.yazdir(x);
+        }
+    </script>
+    </div>
+
+        <div id ="div2" runat="server">
+            MY DIV:
+
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ AccessCodeBehindFromJS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                Button btn = new Button();
+               
+                btn.Text = rnd.Next(1, 999).ToString();
+                btn.Attributes.Add("onclick", "cside('" + btn.Text +"')");
+                div2.Controls.Add(btn);
+              
+            }
+
+
+
+        }
+
+
+
+
+        [System.Web.Services.WebMethod]
+       public static  void yazdir(string ID)
+        {
+            Button lbl = new Button();
+            lbl.Text = "FROM ME : " + ID;
+            Page frm = HttpContext.Current.Handler as Page;
+            frm.FindControl("div2").Controls.Add(lbl);
+          
+        }
+    }
+}
